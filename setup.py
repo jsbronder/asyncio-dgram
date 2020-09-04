b985d336dad83e0cd66ad469cb981316367788a8
Justin Bronder <jsbronder@cold-front.org>
2020-09-04 13:45:44 -0400

    asyncio-dgram-1.1.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if sys.version_info.major == 3 and sys.version_info.minor >= 7:
 
 setuptools.setup(
     name="asyncio-dgram",
-    version="1.1.0",
+    version="1.1.1",
     description="Higher level Datagram support for Asyncio",
     long_description=readfile("README.md"),
     url="https://github.com/jsbronder/asyncio-dgram",
