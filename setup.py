d30acc609f4b6f87af63cb1fbb1475aa17bd06e4
Justin Bronder <jsbronder@cold-front.org>
2021-07-21 10:50:59 -0400

    asyncio-dgram-2.1.0
    - Type hints added
    - DatagramStream.send() renamed to DatagramStream._send().  This should
      be backward compatible as both DatagramServer and DatagramClient were
      overloading it.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ if sys.version_info.major == 3 and sys.version_info.minor >= 7:
 
 setuptools.setup(
     name="asyncio-dgram",
-    version="2.0.0",
+    version="2.1.0",
     description="Higher level Datagram support for Asyncio",
     long_description=readfile("README.md"),
     url="https://github.com/jsbronder/asyncio-dgram",
